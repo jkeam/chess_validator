f3ea20d33b027c1ed867fb2d370d06ac23506366
Jon Keam <jpkeam@gmail.com>
2014-02-08 21:41:06 -0500

    updated build

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 
 tar -cvf jkeam_chess_validator.tar spec Rakefile inputs driver.rb lib run.sh README.md 
 gzip jkeam_chess_validator.tar
-mv jkeam_chess_validator.tar dist
+mv jkeam_chess_validator.tar.gz dist
+echo "Done!"
